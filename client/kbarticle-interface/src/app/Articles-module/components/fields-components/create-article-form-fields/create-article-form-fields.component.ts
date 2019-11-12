1edf4352a9ea75eef36610e05ff375e2aeb2f8fe
saurabh_mahambrey <saurabh_mahambrey@persistent.com>
2019-11-12 09:17:05 +0530

    fixed issue with add field value option

--- client/kbarticle-interface/src/app/Articles-module/components/fields-components/create-article-form-fields/create-article-form-fields.component.ts
+++ client/kbarticle-interface/src/app/Articles-module/components/fields-components/create-article-form-fields/create-article-form-fields.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormArray } from '@angular/forms';
 import { Article_Field } from './../../../classes/article_fields';
+import { validateStyleParams } from '../../../../../../node_modules/@angular/animations/browser/src/util';
 
 @Component({
   selector: 'app-create-article-form-fields',
@@ -9,8 +10,14 @@ import { Article_Field } from './../../../classes/article_fields';
 })
 export class CreateArticleFormFieldsComponent implements OnInit {
 
-  selectedType: number;
+  
+  selectedType: number;                 //current selected field type
+  hasValues: boolean = false;           // current selected field has option to add values or not
 
+
+  /**
+   * Different Available field type options 
+   */
   field_type = [
     {name: 'text', value:1},
     {name: 'textarea', value:2},
@@ -20,8 +27,10 @@ export class CreateArticleFormFieldsComponent implements OnInit {
     {name: 'radiobox', value: 6}
   ];
 
-  value_map=[];
 
+  /**
+   * Article field form built using form builder to create article fields 
+   */
   article_fields_form = this.fb.group({
     // type : [''],
     name : [''],
@@ -31,6 +40,9 @@ export class CreateArticleFormFieldsComponent implements OnInit {
     field_values: this.fb.array([ this.fb.control('')])
   })
 
+  /**
+   * Get Field values as form array
+   */
   get field_values() {
     return this.article_fields_form.get('field_values') as FormArray;
   }
@@ -38,20 +50,29 @@ export class CreateArticleFormFieldsComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
 
-  updateSelectedFieldType(value){
-    if(this.selectedType == 1 || this.selectedType == 2){
-
+  /**
+   * Provide an option to add field values based on the type of field select by the user
+   * 
+   * @param type = type of article form field selected by the user eg. dropdown, text etc.
+   */
+  updateSelectedFieldType(type){
+   
+    this.selectedType = type.value;
+    if(type.value != 1 && type.value != 2){
+      this.hasValues = true;
     }
-    this.selectedType = value.value;
-    console.log(this.selectedType)
+    else{
+      this.hasValues  = false;
+    }
   }
 
-  // enableFieldValueOptions(value){
-  //   if((value == 2) || (value == 3)){
-      
-  //   }
-  // }
 
+
+  /**
+   * Add another could to add value to field_values list
+   * 
+   * @param index = position in field_values.value array for which this function was called
+   */
   addFieldValueCol(index){
     console.log(this.field_values.value)
     if(this.field_values.value[index].length > 0){
@@ -59,15 +80,33 @@ export class CreateArticleFormFieldsComponent implements OnInit {
     }
   }
 
+  /**
+   * Remove value at index 'i' in the field_values.value array
+   * 
+   * @param i = position in field_values.value array for which this function was called
+   */
   removeValue(i){
     if(i!=0)
     this.field_values.removeAt(i);
   }
 
 
-  ngOnInit() {
-    console.log(this.selectedType)
+  /**
+   * Validation function to identify non unique entries in the field_values.value array
+   * 
+   * @param index = position in field_values.value array for which this function was called
+   */
+  validateFieldValues(index){
+    const lenVal = this.field_values.value.filter((val, i) => (val == this.field_values.value[index] && (i != index)))
+    console.log(lenVal)
+    if(lenVal.length > 0){
+      console.log(lenVal)
+      console.log("cannot contain similar values")
+    }
   }
+
+
+  ngOnInit() { }
 
 
 
