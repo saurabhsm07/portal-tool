3cba62d440a8c6e547ccfdfd59832690d6651598
saurabh_mahambrey <saurabh_mahambrey@persistent.com>
2020-06-16 16:28:36 +0530

    UPDATE FIX long pending pagination issue search feature UPDATE article hc search component UPDATE articleSearchByText API controller  method

--- client/help-center-interface/src/app/Helpcenter-module/components/helpcenter-search/helpcenter-search.component.ts
+++ client/help-center-interface/src/app/Helpcenter-module/components/helpcenter-search/helpcenter-search.component.ts
@@ -6,6 +6,7 @@ import { FormBuilder } from '@angular/forms';
 
 import { Article } from './../../../Articles-module/classes/article';
 import { ArticleService } from './../../../Articles-module/services/article-service/article.service';
+import { conditionallyCreateMapObjectLiteral } from '../../../../../node_modules/@angular/compiler/src/render3/view/util';
 
 
 @Component({
@@ -19,6 +20,8 @@ export class HelpcenterSearchComponent implements OnInit {
   articles : Article [];
   articleCount: number;           //count of articles for this search
   articleSet : number;
+
+  pageIndexes : number[];
   constructor(private fb: FormBuilder,
             private articleService: ArticleService,
             private route: ActivatedRoute,
@@ -39,11 +42,13 @@ export class HelpcenterSearchComponent implements OnInit {
 
     this.searchString.setValue(this.route.snapshot.queryParamMap.get('query'));
     this.articleSet = parseInt(this.route.snapshot.queryParamMap.get('offset'));
+    this.pageIndexes = [1,2,3,4,5]
+    this.pageIndexes = this.pageIndexes.map(pageIndex => pageIndex + this.articleSet - 1);
+    console.log(this.pageIndexes)
     this.searchResults.subscribe((results) => {
                               
                               this.articles = results.articles;
-                              this.articleCount = results.count
-                                                        
+                              this.articleCount = results.count;
                             },
                               (error) => {
                                 console.log(error);
@@ -54,13 +59,57 @@ export class HelpcenterSearchComponent implements OnInit {
 
 
   ifNextSetExists(count){
-    if((count+this.articleSet)*10 < this.articleCount)
+    if((count)*10 < this.articleCount)
       return true;
     else
       return false
   }
 
+  /**
+   * get next set of articles from count offset
+   * @param count : the number offset after which articles are to be fetched
+   */
   fetchNextSet(count){
-    this.router.navigate([], {queryParams: {query: this.searchString.value, offset: this.articleSet+ count}})
+    this.router.navigate([], {queryParams: {query: this.searchString.value, offset: count}})
+  }
+
+  /**
+   * increment pagination buttons index
+   * @param index : update pagination list by numeric index
+   */
+  nextPageIndexes(index){
+    if(this.articleSet + index < (this.articleCount/ 10)){
+      this.pageIndexes = this.pageIndexes.map(pageIndex => pageIndex + index).filter( pageIndex => pageIndex < this.articleCount/10)
+      this.router.navigate([], {queryParams: {query: this.searchString.value, offset: this.articleSet+ this.pageIndexes[this.pageIndexes.length - 1]}})
+    }
+    
+    
+  }
+
+  /**
+   * decrement pagination buttons index
+   * @param index : update pagination list by numeric index
+   */
+  prevPageIndexes(index){
+    console.log("test 2")
+    console.log(this.pageIndexes[0])
+    if(this.pageIndexes[0] > 1){
+      if(index == 1){
+        this.pageIndexes = this.pageIndexes.map(pageIndex => pageIndex - index)
+      }
+      else{
+        if((this.pageIndexes[0] - 5) < 0){
+          this.pageIndexes = [1,2,3,4,5]
+        }else{
+          this.pageIndexes = this.pageIndexes.map(pageIndex => pageIndex - index)
+          console.log("condition 3")
+        }
+      }
+      this.router.navigate([], {queryParams: {query: this.searchString.value, offset: this.articleSet+ this.pageIndexes[0]}})
+
+    }else{
+      this.pageIndexes = [1,2,3,4,5]
+      this.router.navigate([], {queryParams: {query: this.searchString.value, offset: this.articleSet+ this.pageIndexes[0]}})
+    }
   }
 }
