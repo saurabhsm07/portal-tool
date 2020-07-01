316dc8e853c8b93afa950d05050fa19b989638ae
saurabh_mahambrey <saurabh_mahambrey@persistent.com>
2020-07-01 18:37:55 +0530

    UPDATE guideadmin layout for all major modules UPDATE category view hc component for product downloads, patches and hotfix page

--- client/help-center-interface/src/app/Category-module/components/view-category-hc/view-category-hc.component.ts
+++ client/help-center-interface/src/app/Category-module/components/view-category-hc/view-category-hc.component.ts
@@ -44,6 +44,7 @@ export class ViewCategoryHcComponent implements OnInit {
   
   //list of category data objects who have custom design templates  and do not follow general structure
   customCategoryTemplates = {
+    documentation : {
     201273833: {
       name: 'Product Manuals',
       childCategoryData: [
@@ -74,6 +75,77 @@ export class ViewCategoryHcComponent implements OnInit {
         {id: '360000464971', tagline: 'Powerful big data analytics platform with an easy-to-use self-service interface for data preparation, analysis, visualization, and machine learning', name: 'Data Analytics', nameIcon: 'assets/images/si-cat-icon.png', backImg: 'assets/images/si-cat-icon.png' },
       ]
   }
+},
+
+download: {
+  201273863:{
+    name: 'hotfix',
+    childCategoryData:[
+      {id:'200146630', name: 'Radia', downloadInfo:[{name:'Version 10.0', link:'hc/en-us/sections/id/202095986'},
+      {name:'Version 10.0', link:'hc/en-us/sections/id/201393746'},
+      {name:'Version 9.1', link:'hc/en-us/sections/id/200415080'},
+      {name:'Version 9.0', link:'hc/en-us/sections/id/200415030'}], seeMore: '/hc/en-us/categories/id/200146190'},
+
+      {id:'360001491012', name: 'Neuro', downloadInfo:[{name:'Neuro 1.2.3', link:'hc/en-us/articles/id/360023325112'}], seeMore: '/hc/en-us/categories/id/360001491012'},                                                                                                                                              
+    ]
+    
+  },
+  201258686: {
+    name: 'patches',
+    childCategoryData:[
+      {id:'200146630', name: 'Radia', downloadInfo:[{name:'Version 10.0 CP2', link:'hc/en-us/articles/id/360002974651'},
+      {name:'Version 10.0', link:'hc/en-us/sections/id/360000008323'},
+      {name:'Version 9.2', link:'hc/en-us/sections/id/115000031223'},
+      {name:'Version 9.1', link:'hc/en-us/sections/id/200781014'}], seeMore: '/hc/en-us/categories/id/200146630'},
+
+      {id:'201186243', name: 'Rovius CloudPlatform', downloadInfo:[{name:'CloudPlatform 4.11', link:'hc/en-us/sections/id/360000008303'},
+      {name:'CloudPlatform 4.7.x.x', link:'hc/en-us/sections/id/203011203'},
+      {name:'CloudPlatform 4.5.x.x', link:'hc/en-us/sections/id/201834696'},
+      {name:'CloudPlatform 4.3.x.x', link:'hc/en-us/sections/id/201828653'}], seeMore: '/hc/en-us/categories/id/201186243'},
+
+      {id:'201181036', name: 'CPBM', downloadInfo:[{name:'CPBM 2.6.0', link:'hc/en-us/articles/id/115001581863'},
+      {name:'CPBM 2.5.0', link:'hc/en-us/sections/id/115000593883'},
+      {name:'CPBM 2.4.0', link:'hc/en-us/sections/id/201848866'},
+      {name:'CPBM 2.3.x', link:'hc/en-us/sections/id/201848876'}], seeMore: '/hc/en-us/categories/id/201181036'},
+
+      {id:'360000282832', name: 'Rovius', downloadInfo:[{name:'Rovius Cloud 1.3.1 Hotfix 01', link:'hc/en-us/articles/id/360002974651'}], seeMore: '/hc/en-us/categories/id/360000282832'},
+    ]
+
+  },
+  201273853:{
+    name:'product',
+    childCategoryData:[
+      {id:'200146230', name: 'Radia', downloadInfo:[{name:'Version 10.0', link:'/hc/en-us/articles/id/208592116'},
+                                                {name:'Version 9.2', link:'/hc/en-us/articles/id/205628844'},
+                                                {name:'Version 9.1', link:'/hc/en-us/articles/id/202755820'},
+                                                {name:'Version 9.0', link:'/hc/en-us/articles/id/202042070'}], seeMore: '/hc/en-us/categories/id/200146230'},
+      {id:'201174236', name: 'Rovius CloudPlatform', downloadInfo:[{name:'CloudPlatform 4.11.0', link:'/hc/en-us/articles/id/115005123863'},
+                                                {name:'CloudPlatform 4.7.1', link:'/hc/en-us/articles/id/217979123'},
+                                                {name:'CloudPlatform 4.7', link:'/hc/en-us/articles/id/208348643'},
+                                                {name:'CloudPlatform 4.5.1', link:'/hc/en-us/articles/id/208251553'}], seeMore: '/hc/en-us/categories/id/201174236'},
+      {id:'201187683', name: 'CPBM', downloadInfo:[{name:'CPBM 2.6.0', link:'/hc/en-us/articles/id/115005552406'},
+                                                {name:'CPBM 2.5.0', link:'/hc/en-us/articles/id/115000576466'},
+                                                {name:'CPBM 2.4.x', link:'/hc/en-us/articles/id/207948126'},
+                                                {name:'CPBM 2.3.X', link:'/hc/en-us/articles/id/208255533'}], seeMore: '/hc/en-us/categories/id/201187683'},
+      {id:'201253086', name: 'Sentient', downloadInfo:[{name:'Sentient 2.0.3', link:'/hc/en-us/articles/id/360001781571'},
+                                                {name:'Sentient 2.0.1', link:'/hc/en-us/articles/id/115001846886'},
+                                                {name:'Sentient 1.1', link:'/hc/en-us/articles/id/212343066'},
+                                                {name:'Sentient 1.0', link:'/hc/en-us/articles/id/207820426'}], seeMore: '/hc/en-us/categories/id/201253086'},
+      {id:'201253086', name: 'Rovius', downloadInfo:[{name:'Rovius Cloud 1.3.1', link:'/hc/en-us/sections/id/360000330932'},
+                                                {name:'Rovius Cloud 1.3', link:'/hc/en-us/sections/id/360000030443'},
+                                                {name:'Rovius Cloud 1.2', link:'/hc/en-us/sections/id/115001358643'}], seeMore: '/hc/en-us/categories/id/115000019143'},
+      {id:'201253086', name: 'ShareInsights', downloadInfo:[{name:'ShareInsights 3.1 Sagittarius', link:'/hc/en-us/articles/id/360025496211'},
+                                                {name:'ShareInsights 3.0 Phoenix', link:'/hc/en-us/articles/id/360021347391'},
+                                                {name:'ShareInsights 2.8.15 Orion', link:'/hc/en-us/articles/id/360016108832'},
+                                                {name:'ShareInsights 2.8.11 Octans', link:'/hc/en-us/articles/id/360007169532'}], seeMore: '/hc/en-us/categories/id/115000760023'},
+      {id:'201253086', name: 'Neuro', downloadInfo:[{name:'Neuro 1.2.3.2', link:'/hc/en-us/articles/id/360025933012'},
+                                                {name:'Neuro 1.2.3.1', link:'/hc/en-us/articles/id/360019806792'},
+                                                {name:'Neuro 1.2.2', link:'/hc/en-us/articles/id/360015481972'},
+                                                {name:'Neuro 1.2.1', link:'/hc/en-us/articles/id/360008697492'}], seeMore: '/hc/en-us/categories/id/360000578592'},
+    ]
+  }
+
+}
   
   }
 
@@ -127,7 +199,7 @@ export class ViewCategoryHcComponent implements OnInit {
    */
   public initializeSectionArticleObject() {
     this.sectionList.forEach((section, index) => {
-      this.sectionArticlesObj[section.id] = { articles: [], see_more: section.html_url, isOpen: false };
+      this.sectionArticlesObj[section.id] = { downloadInfo: [], see_more: section.html_url, isOpen: false };
     });
   }
 
@@ -155,15 +227,51 @@ export class ViewCategoryHcComponent implements OnInit {
   }
 
 /**
- *  method checks if the category is valid or not
+ *  method checks if the category follows generic cateogyr template design or not
  * @param categoryId : id of the category in current hc view
  */
   public isGenericCategory(categoryId){
-    console.log(Object.keys(this.customCategoryTemplates))
-    if(Object.keys(this.customCategoryTemplates).indexOf(categoryId.toString()) == -1)
-      return true;
+    
+    if(((Object.keys(this.customCategoryTemplates.documentation).indexOf(categoryId.toString()) == -1))&&
+        (Object.keys(this.customCategoryTemplates.download).indexOf(categoryId.toString()) == -1))
+      return true
     else
       return false;
 
+  }
+
+  /**
+   *method returns true if required template is of documentation category
+   * @param categoryId id of the current category
+   */
+  public isDocumentationTemplate(categoryId){
+    if(Object.keys(this.customCategoryTemplates.documentation).indexOf(categoryId.toString()) != -1)
+      return true;
+    else
+      return false;
+    
+  }
+
+  /**
+   * method returns true if required template is of download category
+   * @param categoryId id of the current category
+   */
+  public isDownloadTemplate(categoryId){
+    if(Object.keys(this.customCategoryTemplates.download).indexOf(categoryId.toString()) != -1)
+      return true;
+    else
+      return false;
+    
+  }
+
+  /**
+   * 
+   * @param templateType : 
+   * @param categoryId :
+   */
+  public getChildCategories(templateType, categoryId){
+    console.log("child category")
+    console.log(this.customCategoryTemplates[templateType][categoryId].childCategoryData);
+    return this.customCategoryTemplates[templateType][categoryId].childCategoryData;
   }
 }
